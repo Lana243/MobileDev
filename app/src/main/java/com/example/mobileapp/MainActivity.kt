d25a7aaa8fcd5a9cf0471652d1242f29bb299117
Lana243 <lana0771711@gmail.com>
2021-09-24 17:26:49 +0300

    Add user list rendering

--- app/src/main/java/com/example/mobileapp/MainActivity.kt
+++ app/src/main/java/com/example/mobileapp/MainActivity.kt
@@ -2,10 +2,73 @@ package com.example.mobileapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val recyclerView = findViewById<RecyclerView>(R.id.userRecycleView)
+        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        val adapter = UserAdapter()
+        recyclerView.adapter = adapter
+        adapter.userList = loadUsers()
+        adapter.notifyDataSetChanged()
+    }
+
+    private fun loadUsers(): List<User> {
+        return listOf(
+            User(
+                avatarUrl = "",
+                userName = "User name 0",
+                groupName = "A"
+            ),
+            User(
+                avatarUrl = "",
+                userName = "User name 1",
+                groupName = "A"
+            ),
+            User(
+                avatarUrl = "",
+                userName = "User name 2",
+                groupName = "A"
+            ),
+            User(
+                avatarUrl = "",
+                userName = "User name 3",
+                groupName = "A"
+            ),
+            User(
+                avatarUrl = "",
+                userName = "User name 4",
+                groupName = "B"
+            ),
+            User(
+                avatarUrl = "",
+                userName = "User name 5",
+                groupName = "B"
+            ),
+            User(
+                avatarUrl = "",
+                userName = "User name 6",
+                groupName = "B"
+            ),
+            User(
+                avatarUrl = "",
+                userName = "User name 7",
+                groupName = "C"
+            ),
+            User(
+                avatarUrl = "",
+                userName = "User name 8",
+                groupName = "C"
+            ),
+            User(
+                avatarUrl = "",
+                userName = "User name 9",
+                groupName = "C"
+            )
+        )
     }
 }
---
+++ app/src/main/java/com/example/mobileapp/User.kt
@@ -0,0 +1,7 @@
+package com.example.mobileapp
+
+data class User(
+    val avatarUrl: String, // E.g. "https://mydomain.com/user_1_avatar.jpg"
+    val userName: String,
+    val groupName: String
+)
