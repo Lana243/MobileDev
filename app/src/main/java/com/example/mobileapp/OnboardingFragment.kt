235376f09f263cbabbfcf51e76cacded61e329d2
Lana243 <lana0771711@gmail.com>
2021-10-13 00:52:14 +0300

    add onboarding fragment

--- app/src/main/java/com/example/mobileapp/OnboardingFragment.kt
+++ app/src/main/java/com/example/mobileapp/OnboardingFragment.kt
@@ -1,4 +1,40 @@
 package com.example.mobileapp
 
-class OnboardingFragment {
+import android.os.Bundle
+import android.view.View
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.example.mobileapp.databinding.FragmentOnboardingBinding
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.SimpleExoPlayer
+
+class OnboardingFragment : BaseFragment(R.layout.fragment_onboarding) {
+
+    val viewBinding by viewBinding(FragmentOnboardingBinding::bind)
+
+    private var player: ExoPlayer? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        player = SimpleExoPlayer.Builder(requireContext()).build().apply {
+            addMediaItem(MediaItem.fromUri("assets:///onboarding.mp4"))
+            repeatMode = Player.REPEAT_MODE_ALL
+            prepare()
+        }
+        viewBinding.playerView.player = player
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
 }
